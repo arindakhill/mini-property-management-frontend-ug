267eec7ef23700c933a7516523a68fd9a160564c
arindakhill <arinda.hillary@gmail.com>
2024-02-08 18:54:12 -0600

    completed most customer related features

--- src/components/Houses/HouseItem.jsx
+++ src/components/Houses/HouseItem.jsx
@@ -16,18 +16,48 @@ import {
 import { BiBed, BiBath, BiArea } from "react-icons/bi";
 import { StarIcon } from "@chakra-ui/icons"; // Import the Star icon for the favorites button
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useAuth } from "../../context/AuthContext";
+import { useContext, useState, useEffect } from "react";
 
 
-const HouseItem = ({ house ,user }) => {
+
+const HouseItem = ({ house }) => {
   const toast = useToast();
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || {};
-  const userFavorites = favorites[user?.id] || [];
-  const isFavorite = userFavorites.includes(house.id);
+  const { user } = useAuth; // Using AuthContext to access the current user
+  const [isFavorite, setIsFavorite] = useState(false);
+  const styles = useStyleConfig("HouseItem");
 
-  const addToFavorites = (houseId) => {
+  useEffect(() => {
+    // Function to check if the current property is in the user's favorite list
+    const checkFavoriteStatus = async () => {
+      if (user) {
+        try {
+          const response = await axios.get(`http://localhost:8080/api/v1/properties/favourite-properties`, {
+            headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` } // Assuming you have the token in your user context
+          });
+          const favoriteProperties = response.data;
+          setIsFavorite(favoriteProperties.some(favoriteProperty => favoriteProperty.id === house.id));
+        } catch (error) {
+          console.error('Error fetching favorite properties:', error);
+          //use toast to show error message
+          toast({
+            title: 'Error fetching favorite properties',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          });
+        }
+      }
+    };
+
+    checkFavoriteStatus();
+  }, [house.id, user]);
+
+  const toggleFavorite = async () => {
     if (!user) {
       toast({
-        title: 'You must be signed in to add favorites.',
+        title: 'You must be signed in to modify favorites.',
         status: 'warning',
         duration: 5000,
         isClosable: true,
@@ -35,30 +65,51 @@ const HouseItem = ({ house ,user }) => {
       return;
     }
 
-    if (isFavorite) {
+    try {
+      const url = `http://localhost:8080/api/v1/properties/${house.id}/unlike`;
+      if (isFavorite) {
+        // Call unlike endpoint
+        await axios.post(`http://localhost:8080/api/v1/properties/${house.id}/unlike`, {
+          headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}`  },
+        });
+        setIsFavorite(false);
+        toast({
+          title: 'Removed from Favorites',
+          description: 'This property has been removed from your favorites.',
+          status: 'info',
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        // Call like endpoint
+        await axios.post(`http://localhost:8080/api/v1/properties/${house.id}/like`, {}, {
+          headers: { Authorization:  `Bearer ${sessionStorage.getItem('token')}`  },
+        });
+        setIsFavorite(true);
+        toast({
+          title: 'Added to Favorites',
+          description: 'This property has been added to your favorites.',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      console.error('Error modifying favorite status:', error);
       toast({
-        title: 'Property already in favorites.',
-        status: 'info',
+        title: 'Error',
+        description: 'An error occurred while modifying favorite status.',
+        status: 'error',
         duration: 5000,
         isClosable: true,
       });
-      return;
     }
-
-
-    userFavorites.push(houseId);
-    favorites[user.id] = userFavorites;
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-    toast({
-      title: 'Added to Favorites',
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    });
   };
-  
 
-  const styles = useStyleConfig("HouseItem");
+
+
+
+ 
 
 
   return (
@@ -95,7 +146,7 @@ const HouseItem = ({ house ,user }) => {
             </Heading>
 
             <Text fontSize="13px" color="grey">
-             {house.address}
+             {house.address.country} {house.address.city} {house.address.street} {house.address.zipCode}
             </Text>
 
             <Divider my="2.5" />
@@ -103,12 +154,12 @@ const HouseItem = ({ house ,user }) => {
             <HStack spacing="5">
             <HStack>
                 <BiBed style={{ color: "#D53F8C" }} />
-                <Text fontSize="12px">{house.bedrooms}</Text>
+                <Text fontSize="12px">{house.bedRooms}</Text>
             </HStack>
 
             <HStack>
                 <BiBath style={{ color: "#D53F8C" }} />
-                <Text fontSize="12px">{house.bathrooms}</Text>
+                <Text fontSize="12px">{house.bathRooms}</Text>
             </HStack>
 
             <Button leftIcon={isFavorite ? <StarIcon /> : <StarIcon />} 
