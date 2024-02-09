a9ba2efe7bfbc1534e89c255d5713d2e71ea5937
tonny-2022 <tonny78k@gmail.com>
2024-02-08 18:55:57 -0600

    updates from tonny2/8/2024

--- src/components/Owners/OwnerDetails.jsx
+++ src/components/Owners/OwnerDetails.jsx
@@ -21,27 +21,23 @@ export default function OwnerDetails(){
                <h1>Owner Details page</h1>
                <div className="form-group">
                 <label>Owner Id:</label>
-                    <input type="text" className="form-control col-sm-6" name="id" value={owner?.id} readOnly/>
+                    <input type="text" className="form-control col-sm-6" name="id" value={owner.id} readOnly/>
                 </div>
                 <div className="form-group">
                 <label>First Name:</label>
-                    <input type="text" className="form-control col-sm-6" name="firstname" value={owner?.firstName} readOnly/>
+                    <input type="text" className="form-control col-sm-6" name="firstname" value={owner?.user.firstname} readOnly/>
                 </div>
                 <div className="form-group">
                 <label>Last name:</label>
-                <input type="text" className="form-control col-sm-6 mb-3" name="lastname" value={owner?.lastName}  readOnly/>
+                <input type="text" className="form-control col-sm-6 mb-3" name="lastname" value={owner.user.lastname}  readOnly/>
               </div>
               <div className="form-group">
                 <label>Email:</label>
-                <input type="text" className="form-control col-sm-6 mb-3" name="email"  value={owner?.email} readOnly/>
-              </div>
-              <div className="form-group">
-                <label>Username:</label>
-                <input type="text" className="form-control col-sm-6 mb-3" name="username" value={owner.username} readOnly/>
+                <input type="text" className="form-control col-sm-6 mb-3" name="email"  value={owner.user.email} readOnly/>
               </div>
               <div className="form-group mb-3">
                 <label>Status:</label>
-                    <input type="text" className="form-control col-sm-6" name="status" value={owner?.status} readOnly/>
+                    <input type="text" className="form-control col-sm-6" name="status" value={owner.isenabled} readOnly/>
                 </div>
             <div className="form-group d-flex">
             <div className='me-2'>
--- src/components/Owners/PropertyList.jsx
+++ src/components/Owners/PropertyList.jsx
@@ -5,10 +5,10 @@ import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 import 'datatables.net-bs5'
 import { getPropertiesApi } from "../../Api/Api";
 import  {jwtDecode} from "jwt-decode";
+import axios from "axios";
+
 export default function PropertyList(){
     const token = sessionStorage.getItem('token');
-    console.log(token);
-  
     const navigate=useNavigate();
     
   
@@ -16,83 +16,50 @@ export default function PropertyList(){
     const [selectedProperty, setSelectedProperty] = useState([]);
     const [propertyList, setpropertyList] = useState([]);
     const[deletMsg,setDeleteMsg]=useState(null);
-    const properties = [
-        {
-
-            id:1,
-            category: 'For rent',
-            type: 'Apartment',
-            country: 'USA',
-            sddress: '123 Main St',
-            state: 'New York',
-            city: 'New York City',
-            PostalCode: '10001',
-            amount: '$1500',
-            description: 'Cozy apartment with great view',
-            bathroomsNumber: 2,
-            bedroomsNumber: 1,
-            ownerId:1
-        },
-        {
-            id:2,
-            category: 'For Sale',
-            type: 'Condominium',
-            country: 'Canada',
-            cddress: '456 Maple Ave',
-            state: 'Ontario',
-            city: 'Toronto',
-            costalCode: 'M5V 3M3',
-            amount: '$300000',
-            description: 'Spacious condo with modern amenities',
-            bathroomsNumber: 1,
-            dedroomsNumber: 2,
-            ownerId:2
-        },
-        {
-            id:3,
-            category: 'For rent',
-            type: 'House',
-            country: 'UK',
-            address: '789 Elm St',
-            state: 'London',
-            postalCode: 'SW1A 1AA',
-            amount: '£2000',
-            description: 'Charming house in central London',
-            bathroomsNumber: 3,
-            bedroomsNumber: 4,
-            ownerId:2
-        },
-       
-    ];
+    
       useEffect(()=>{
         const dataTable = $('#tblProperties').DataTable(); 
-        getAllProperties(); 
-      } );
+       
+       getallPropertiesby();
+      } ,[]);
 
-
-   
-   
-      const getAllProperties=()=>{
-
-
-
-        getPropertiesApi().then(response=>{
-            propertyList(response.data);  
-            console.log(propertyList)
+      
+    
+      const getallPropertiesby = () => {
+        return axios.get(`http://localhost:8080/api/v1/properties/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         })
+        .then(response => {
+          const propertyData = response.data;
+          setpropertyList(propertyData);
+        })
+        .catch(error => {
+          throw error;
+        });
+      };
+   
+   
+    /* const getAllProperties=(tkn)=>{
+
+        getPropertiesApi(tkn).then(response=>{
+            propertyList(response.data);  
+           console.log(propertyList)
+       })
         
         .catch(error=>console.log(error))
-    }
+    }*/
   
        const   goToPropertyDetailPage=(propId)=>{
-        const selectedProp= properties.find((prop)=>prop.id===propId);
+        const selectedProp= propertyList.find((prop)=>prop.id===propId);
          setSelectedProperty(selectedProp)
          navigate('/property-detail',{ state: { prop: selectedProp } });
 
       }
    
       const   goToPropertyEditPage=(propId)=>{
-        const selectedProp= properties.find((prop)=>prop.id===propId);
+        const selectedProp= propertyList.find((prop)=>prop.id===propId);
         setSelectedProperty(selectedProp)
          navigate('/update-property',{ state: { prop: selectedProp } });
 
@@ -108,7 +75,7 @@ export default function PropertyList(){
 
             <h3 style={{ textAlign: 'center', fontSize:'25px' }} >List of  Properties</h3><hr />
         
-            <div class="table-responsive ">
+            <div className="table-responsive ">
             <table className="table" id="tblProperties">
             
             <thead>
@@ -128,8 +95,8 @@ export default function PropertyList(){
                 row=>(
                     <tr key={row.id}>
                     <td>{row.id}</td>
-                    <td>{row.firstname}</td>
-                    <td>{row.lastname}</td>
+                    <td>{row.name}</td>
+                    <td>{row.description}</td>
                     <td>{row.listing_type}</td>
                     <td>{row.property_type}</td>
                     <td><button type="button" className="btn btn-success btn-flat"onClick={()=>goToPropertyDetailPage(row.id)} >Details</button></td>   
